396c9e1b6a149dc6447540c700278e8bafd3eb23
hany alhabboby <hany89@live.se>
2024-02-13 13:27:36 +0100

    Initial commit

---
+++ src/pages/Home.js
@@ -0,0 +1,10 @@
+import BookDisplay from "../components/BookDisplay";
+
+export default function Home(){
+    return(
+        <div>
+            <p>This is the welcome page</p>
+            <BookDisplay/>
+        </div>
+    );
+}
